b4d82664c9ce522b079e9b67d111b06c5c58b89f
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-05-14 22:45:44 +0000

    added timestamp

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask, redirect
 
 
@@ -8,7 +9,8 @@ messages = []
 
 def add_messages(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 
 def get_all_messages():
